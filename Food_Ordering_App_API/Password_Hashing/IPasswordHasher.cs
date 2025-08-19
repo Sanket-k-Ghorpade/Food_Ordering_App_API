3787b673e9e222bc296ef5d82c0421512b109d6c
Sanket Ghorpade <126217429+Sanket-k-Ghorpade@users.noreply.github.com>
2025-08-19 00:13:11 -0700

    Delete Food_Ordering_App_API/Password_Hashing directory

--- Food_Ordering_App_API/Password_Hashing/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Food_Ordering_App_API.Password_Hashing
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool VerifyPassword(string password, string hashedPassword);
-    }
-}
